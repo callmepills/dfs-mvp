0ed5d196fc4165cf68b205b0fb8698e9b9ab2c53
Anthony Leveillee <aleveillee@gmail.com>
2017-09-16 10:36:55 -0400

    Create tabs by position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Player } from './player';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  playerList: Object[];
+  quarterBacks: Player[];
+  runningBacks: Player[];
+  wideReceivers: Player[];
+  tightEnds: Player[];
+  defenses: Player[];
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
+    this.quarterBacks = [];
+    this.runningBacks = [];
+    this.wideReceivers = [];
+    this.tightEnds = [];
+    this.defenses = [];
+
     this.http.get('/lineup/getavailableplayers?contestTypeId=21&draftGroupId=15221')
       .subscribe(data => {
-        this.playerList = data['playerList'];
+        data['playerList'].forEach((player) => {
+          switch (player.pn) {
+            case 'QB': this.quarterBacks.push(player); break;
+            case 'RB': this.runningBacks.push(player); break;
+            case 'WR': this.wideReceivers.push(player); break;
+            case 'TE': this.tightEnds.push(player); break;
+            case 'DST': this.defenses.push(player); break;
+          }
+        });
       });
   }
 }
---
+++ src/app/player.ts
@@ -0,0 +1,6 @@
+export class Player {
+  pn;
+  fn;
+  ln;
+  s;
+}
