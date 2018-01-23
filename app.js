063b2e00d09a25a7968f5ce3e6fb513347e0c78b
Anthony Leveillee <aleveillee@gmail.com>
2018-01-23 00:22:41 -0500

    fix proxy paths in the prod app

--- app.js
+++ app.js
@@ -7,17 +7,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.use('/draftkings', proxy('https://www.draftkings.com', {
-  proxyReqPathResolver: function (req) {
-    return '/lineup' + require('url').parse(req.url).path;
-  }
-}));
+app.use('/draftkings', proxy('https://www.draftkings.com'));
 
-app.use('/espn-rankings', proxy('https://espn-rankings.herokuapp.com', {
-  proxyReqPathResolver: function (req) {
-    return '/rankings' + require('url').parse(req.url).path;
-  }
-}));
+app.use('/espn-rankings', proxy('https://espn-rankings.herokuapp.com'));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
