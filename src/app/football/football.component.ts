d4caaf93e0c79f680985220e63b7e837b7752933
Anthony Leveillee <aleveillee@gmail.com>
2018-09-30 12:02:04 -0400

    bump draft group id for week 4

--- src/app/football/football.component.ts
+++ src/app/football/football.component.ts
@@ -48,7 +48,7 @@ export class FootballComponent implements OnInit {
           this.http.get(`/espn-rankings/football/${season}/${week}/wr`),
           this.http.get(`/espn-rankings/football/${season}/${week}/te`),
           this.http.get(`/espn-rankings/football/${season}/${week}/dst`),
-          this.http.get('/draftkings/lineup/getavailableplayers?contestTypeId=21&draftGroupId=21505')
+          this.http.get('/draftkings/lineup/getavailableplayers?contestTypeId=21&draftGroupId=21720')
         ).subscribe((response: Object) => {
 
           this.qbRankings = response[0] as Ranking[];
