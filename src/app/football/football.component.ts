87cd3da8b814e98da0b6bf8d8f695256275aa9b0
Anthony Leveillee <aleveillee@gmail.com>
2020-09-23 19:11:30 -0400

    bump draft group for week 3

--- src/app/football/football.component.ts
+++ src/app/football/football.component.ts
@@ -48,7 +48,7 @@ export class FootballComponent implements OnInit {
           this.http.get(`/espn-rankings/football/${season}/${week}/wr`),
           this.http.get(`/espn-rankings/football/${season}/${week}/te`),
           this.http.get(`/espn-rankings/football/${season}/${week}/dst`),
-          this.http.get('/draftkings/lineup/getavailableplayers?contestTypeId=21&draftGroupId=39754')
+          this.http.get('/draftkings/lineup/getavailableplayers?contestTypeId=21&draftGroupId=39913')
         ]).subscribe((response: Object) => {
 
           this.qbRankings = response[0] as Ranking[];
